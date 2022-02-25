68d5f606abae93c98af8ded797544a1fad4f0c0b
lynn0217 <2579234709@qq.com>
2022-02-25 16:34:39 +0800

    second

---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="login_container">登录组件</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang='less' scoped>
+#login_container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e1e1e;
+}
+</style>

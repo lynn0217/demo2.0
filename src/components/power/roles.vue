9194dd0d3e2c840fd0fe17addf56b929b1b98cf9
lynn0217 <2579234709@qq.com>
2022-03-03 20:54:20 +0800

    角色权限功能

--- src/components/power/roles.vue
+++ src/components/power/roles.vue
@@ -12,6 +12,53 @@
       <el-button type="primary" @click="addRolesDialog = true">添加角色</el-button>
       <!-- 表格区域 -->
       <el-table :data="rolesList" stripe style="width: 100%" border>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['bdbottom',i1 === 0 ? 'bdtop' : '','vcenter']"
+              v-for="(item1,i1) in scope.row.children"
+              :key="item1.id"
+            >
+              <!-- 一级权限 -->
+              <el-col :span="5">
+                <el-tag closable @close="closeTagById(scope.row,item1.id)">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 二，三级级权限-->
+              <el-col :span="19">
+                <!-- for循环渲染二三级 -->
+                <el-row
+                  :class="[i2===0 ?'':'bdtop','vcenter']"
+                  v-for="(item2,i2) in item1.children"
+                  :key="item2.id"
+                >
+                  <el-col :span="6">
+                    <el-tag
+                      type="success"
+                      closable
+                      @close="closeTagById(scope.row,item2.id)"
+                    >{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag
+                      type="warning"
+                      v-for="(item3) in item2.children"
+                      :key="item3.id"
+                      closable
+                      @close="closeTagById(scope.row,item3.id)"
+                    >{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+            <!-- <pre>
+            {{scope.row}}
+            </pre>-->
+          </template>
+        </el-table-column>
+        <!-- 索引列 -->
         <el-table-column type="index" label="序号"></el-table-column>
         <el-table-column prop="roleName" label="角色名称"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
@@ -23,7 +70,7 @@
               @click="editRoles(scope.row.id)"
               size="mini"
             >编辑</el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="delRolesList()">删除</el-button>
             <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
           </template>
         </el-table-column>
@@ -127,10 +174,56 @@ export default {
       this.editRolesDialog = false
       // 刷新数据列表
       this.getRolesList()
+    },
+    // 删除角色
+    async delRolesList(id) {
+      const { data: res } = await this.$http.delete('roles/' + id)
+      // console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败')
+      }
+      this.getRolesList()
+    },
+    async closeTagById(role, rightId) {
+      // 弹框提示用户是否删除
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该文件, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      const { data: res } = await this.$http.delete(
+        `roles/${role.id}/rights/${rightId}`
+      )
+      // console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除权限失败')
+      }
+      role.children = res.data
+      // this.getRolesList()
     }
   }
 }
 </script>
 
 <style lang='less' scoped>
+.el-tag {
+  margin: 10px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
+}
 </style>
